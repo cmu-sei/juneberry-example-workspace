1c963ddb6f66d825b36cd5e651e46a927e8d585f
hmmoore14 <hmmoore@sei.cmu.edu>
2022-09-26 06:05:50 -0400

    fixing documentation, cleaned up comments

--- docs/mnistod/README.md
+++ docs/mnistod/README.md
@@ -15,57 +15,12 @@ The mnistod repository has a script that allows a user to create reproducible da
 
 1. We first need to generate our train/test/val datasets. 
 
-For this example we will do a 80/10/10 split respectively.
+For this example we will do a 1000/250/250 split respectively.
 
-Training Set:
+Generate train/test/val datasets and annotations:
 `
-python mnistod/make_data.py --data_root /dataroot --images_num 80 --images_path mnistod/Images80_train/ --coco_json mnistod/annotations_train.json --seed 123
-`
+python scripts/mnistod.py --data_root /dataroot --images_path /dataroot/mnistod/images/ --train_split 1000 --test_split 250 --val_split 250 --seed 123`
 
-Validation Set:
-`
-python mnistod/make_data.py --data_root /dataroot --images_num 10 --images_path mnistod/Images10_val/ --coco_json mnistod/annotations_val.json --seed 456
-`
-
-Test Set:
-`
-python mnistod/make_data.py --data_root /dataroot --images_num 10 --images_path mnistod/Images10_test/ --coco_json mnistod/annotations_test.json --seed 789
-`
-
-2. Update paths for newly created datasets
-
-`data_sets/mnistod_train.json`
-
-`"image_data": {
-        "sources": [
-            {
-                "directory": "mnistod/Images80_train.json"
-            }
-        ]
-}
-`
-
-`data_sets/mnistod_val.json`
-
-`"image_data": {
-        "sources": [
-            {
-                "directory": "mnistod/Images10_val.json"
-            }
-        ]
-}
-`
-
-`data_sets/mnistod_test.json`
-
-`"image_data": {
-        "sources": [
-            {
-                "directory": "mnistod/Images10_test.json"
-            }
-        ]
-}
-`
 
 # Run training and evaluation
 `jb_train -w /juneberry-example-workspace mnistod_R50 `
--- scripts/mnistod.py
+++ scripts/mnistod.py
@@ -11,38 +11,46 @@
 
 import os
 import cv2
+import PIL
 import numpy as np
 import shutil
 import random
 import sys
 import json
 import argparse
+from pathlib import Path
+from collections import defaultdict
+import torchvision.datasets
 
 # ================================================================
 # Changelog
 # sei-hmmoore 2022-09-15 - Wrapped the parser into a main() function,
-#                            Wrapped the logic into function outside of main,
-#                            Added in ability to output coco.json format
+#                          Wrapped the logic into function outside of main,
+#                          Added in ability to output coco.json format,
+#                          Generates train/test/val splits in one call
 #
 # ================================================================
 
 
-# sei-amellinger 2022-08-17
 # We use a global randomizer instance instead of random
 randomizer = random.Random()
 
 
+# function that generates the mnist dataset and their train/test/val annotation files for Juneberry
 def labels_and_images(data_root, image_paths, images_path, train_split,
                       test_split, val_split, sizes, image_size, max_iou):
 
+    # pre-defined annotation file names
     annotations = ["annotations_train.json",
                    "annotations_test.json",
                    "annotations_val.json"]
 
+    # user defined splits
     splits = [train_split,
               test_split,
               val_split]
 
+    # mnist category list
     categories = [
         {"id": 0, "name": 'Zero'},
         {"id": 1, "name": 'One'},
@@ -55,65 +63,68 @@ def labels_and_images(data_root, image_paths, images_path, train_split,
         {"id": 8, "name": 'Eight'},
         {"id": 9, "name": 'Nine'}]
 
+    data_rt = Path(data_root)
+
     bboxes_num = 0
+    # iterate over our train/test/val split annotation files
     for i in range(len(annotations)):
+        print(f"Generating: {annotations[i]}")
         image_num = 0
+        # coco.json annotation format
         result = {'annotations': [], 'categories': categories, 'images': []}
+        # iterate over the length of each split for train/test/val
         while image_num < splits[i]:
             blanks = np.ones(shape=[image_size, image_size, 3]) * 255
             data = blanks
             bboxes = [[0, 0, 1, 1]]
-            new_image_path = os.path.realpath(os.path.join(images_path, "%06d.jpg" % (image_num + 1)))
+            img_pth = Path(images_path)
+            new_image_path = os.path.realpath(os.path.join(data_rt, img_pth,
+                                                           "%06d.jpg" % (image_num + 1)))
 
             # small object
             ratios = [0.5, 0.8]
-            # 2022-08-17 - Switched to seeded randomizer
             N = randomizer.randint(0, sizes[0])
             if N != 0: bboxes_num += 1
             for _ in range(N):
-                # 2022-08-17 - Switched to seeded randomizer
                 ratio = randomizer.choice(ratios)
                 idx = randomizer.randint(0, 54999)
-
-                data = make_image(result, blanks, bboxes, image_paths[idx], new_image_path,
+                data = make_image(result, blanks, bboxes, data_rt, image_paths[idx], new_image_path,
                                   image_size, image_num, bboxes_num, max_iou, ratio)
 
             # medium object
             ratios = [1., 1.5, 2.]
-            # 2022-08-17 - Switched to seeded randomizer
             N = randomizer.randint(0, sizes[1])
             if N != 0: bboxes_num += 1
             for _ in range(N):
-                # 2022-08-17 - Switched to seeded randomizer
                 ratio = randomizer.choice(ratios)
                 idx = randomizer.randint(0, 54999)
-                data = make_image(result, blanks, bboxes, image_paths[idx], new_image_path,
+                data = make_image(result, blanks, bboxes, data_rt, image_paths[idx], new_image_path,
                                   image_size, image_num, bboxes_num, max_iou, ratio)
 
             # big object
             ratios = [3., 4.]
-            # 2022-08-17 - Switched to seeded randomizer
             N = randomizer.randint(0, sizes[2])
             if N != 0: bboxes_num += 1
             for _ in range(N):
-                # 2022-08-17 - Switched to seeded randomizer
                 ratio = randomizer.choice(ratios)
                 idx = randomizer.randint(0, 54999)
-                data = make_image(result, blanks, bboxes, image_paths[idx], new_image_path,
+                data = make_image(result, blanks, bboxes, data_rt, image_paths[idx], new_image_path,
                                   image_size, image_num, bboxes_num, max_iou, ratio)
 
             if bboxes_num == 0: continue
+            # create new image
             cv2.imwrite(new_image_path, data)
 
             image_num += 1
 
-        path = data_root + "/mnistod/"
+        path = data_root + '/mnistod/'
+        # generate annotation file
         with open(os.path.join(path, annotations[i]), "w+") as wf:
             json_object = json.dumps(result)
             wf.write(json_object)
+    print("Completed image and annotation generation")
 
 
-# 2022-08-17 - added randomizer setup function
 def setup_randomizer(seed):
     global randomizer
     if seed is None:
@@ -125,6 +136,7 @@ def setup_randomizer(seed):
 
     print(f"Using random seed: {seed}")
     randomizer.seed(seed)
+
 
 def compute_iou(box1, box2):
     """xmin, ymin, xmax, ymax"""
@@ -141,16 +153,19 @@ def compute_iou(box1, box2):
     return ((xmax - xmin) * (ymax - ymin)) / (A1 + A2)
 
 
-def make_image(result, blank, boxes, image_path, new_image_path,
+def make_image(result, blank, boxes, data_rt, image_path, new_image_path,
                image_size, image_num, bbox_num, max_iou, ratio:float=1.0):
+
     ID = image_path.split("/")[-1][0]
     image = cv2.imread(image_path)
     image = cv2.resize(image, (int(28 * ratio), int(28 * ratio)))
     h, w, c = image.shape
+    rel_img_path = Path(new_image_path)
+    rel_img_path = rel_img_path.relative_to(data_rt)
 
     img_obj = {
         "id": image_num + 1,
-        "file_name": new_image_path,
+        "file_name": str(rel_img_path),
         "height": image_size,
         "width": image_size
     }
@@ -165,31 +180,35 @@ def make_image(result, blank, boxes, image_path, new_image_path,
     }
 
     while True:
-        # 2022-08-17 - Switched to seeded randomizer
-        # NOTE: We switched to using a python randomizer instead of numpy
+        # generate random bbox
         xmin = randomizer.randint(0, image_size - w)
         ymin = randomizer.randint(0, image_size - h)
-
         xmax = xmin + w
         ymax = ymin + h
 
+        # bbox format to calculate IOU with other objects in image
         iou_box = [xmin, ymin, xmax, ymax]
 
+        # calculate bbox in coco.json format
         width = (xmax - xmin)
         height = (ymax - ymin)
         area = height * width
         x_center = (xmin + xmax) / 2
         y_center = (ymin + ymax) / 2
-        box = [int(x_center), int(y_center), width, height]
+        # [x,y,width,height] x,y are the center coordinates)
+        bbox = [int(x_center), int(y_center), width, height]
 
         iou = [compute_iou(iou_box, b) for b in boxes]
 
+        # Verify that this new addition to the image is under our IOU limit
         if max(iou) < max_iou:
-            labels_obj["bbox"] = box
+            labels_obj["bbox"] = bbox
             boxes.append(iou_box)
             labels_obj["area"] = area
+            # append the image path if it is not already defined
             if img_obj not in result["images"]:
                 result["images"].append(img_obj)
+            # append the annotation in coco format
             result["annotations"].append(labels_obj)
             break
 
@@ -203,32 +222,21 @@ def make_image(result, blank, boxes, image_path, new_image_path,
     return blank
 
 
-# sei-hmmoore 2022-09-19 - refactor make_data.py,
-#                          remove labels.txt output,
-#                          train/val/test splitting compressed to a single call
-# sei-hmmoore 2022-09-15 - wrapped main logic into main() function
 def main():
     parser = argparse.ArgumentParser()
-    # sei-hmmoore 2022-09-19 - added data_root argument
+
     parser.add_argument("--data_root", type=str, default="/dataroot")
+    parser.add_argument("--annotations_path", type=str, default="./mnistod/")
+    parser.add_argument("--images_path", type=str, default="./mnistod/images/")
     parser.add_argument("--images_num", type=int, default=1000)
     parser.add_argument("--image_size", type=int, default=416)
-    parser.add_argument("--images_path", type=str, default="./mnistod/images/")
-    # sei-hmmoore 2022-09-20 - adding the ability to configure the maximum IOU when creating the images
     parser.add_argument("--max_iou", type=float, default=0.02)
-    # sei-hmmoore 2022-09-19 - removing labels.txt output, adjusted --coco_json to --annotations_path
-    # parser.add_argument("--labels_txt", type=str, default="./yymnist/labels.txt")
-    # sei-hmmoore 2022-09-15
-    parser.add_argument("--annotations_path", type=str, default="./mnistod/")
     parser.add_argument("--small", type=int, default=3)
     parser.add_argument("--medium", type=int, default=6)
     parser.add_argument("--big", type=int, default=3)
-    # sei-hmmoore 2022-09-19 - adding ability to define all splits in a single call
     parser.add_argument("--train_split", type=int, default=80)
     parser.add_argument("--val_split", type=int, default=10)
     parser.add_argument("--test_split", type=int, default=10)
-
-    # sei-amellinger 2022-08-17
     parser.add_argument("--seed", type=int, default=None,
                         help="Seed to use for randomization. Must be 32 bit unsigned for numpy. "
                              "By default, one will be made for you.")
@@ -243,20 +251,22 @@ def main():
     if os.path.exists(flags.images_path): shutil.rmtree(flags.images_path)
     os.mkdir(flags.images_path)
 
+    # Download MNIST dataset from torchvision
+    # mn = torchvision.datasets.MNIST('./mnistod/mnist', download=True)
+    # all_items = defaultdict(list)
+    # for item in mn:
+    #     all_items[item[1]].append(item[0])
+    #
+    # # Show the counts
+    # for k, v in all_items.items():
+    #     print(f"{k} - {len(v)}")
+
     image_paths = [os.path.join(os.path.realpath("."), "./mnistod/mnist/train/" + image_name) for image_name in
                    os.listdir("./mnistod/mnist/train")]
     image_paths += [os.path.join(os.path.realpath("."), "./mnistod/mnist/test/" + image_name) for image_name in
                     os.listdir("./mnistod/mnist/test")]
 
-    # sei-hmmoore 2022-09-19 - removed labels.txt output
-    # Generate labels.txt
-    # generate_txt(flags.labels_txt, flags.images_num, flags.images_path, image_paths, sizes, image_size)
-
-    # sei-hmmoore 2022-09-19 - removed txt_to_coco call
-    # Generate coco.json
-    # txt_to_coco(flags.data_root, flags.labels_txt, flags.coco_json)
-
-    # sei-hmmoore 2022-09-19 - creating a new function for the main routine of this script
+    # Generate random images and their corresponding annotation for train/test/val
     labels_and_images(flags.data_root, image_paths, flags.images_path, flags.train_split,
                       flags.test_split, flags.val_split, sizes, image_size, flags.max_iou)
 
